ee617a3ec2ff6ae8265087e0ac575c84e91c0d08
atomcode350 <atomcode350@github.com>
2026-04-02 00:09:04 -0500

    deploy pods

--- infra/iam.tf
+++ infra/iam.tf
@@ -163,9 +163,15 @@ resource "aws_iam_role" "backend_secrets" {
       {
         Effect = "Allow"
         Principal = {
-          Service = "pods.eks.amazonaws.com"
+          Federated = module.eks.oidc_provider_arn
         }
-        Action = ["sts:AssumeRole", "sts:TagSession"]
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:aud" = "sts.amazonaws.com"
+            "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub" = "system:serviceaccount:${local.k8s_namespace}:backend-secrets-sa"
+          }
+        }
       }
     ]
   })
@@ -176,12 +182,5 @@ resource "aws_iam_role" "backend_secrets" {
 resource "aws_iam_role_policy_attachment" "backend_secrets_access" {
   role       = aws_iam_role.backend_secrets.name
   policy_arn = aws_iam_policy.fargate_secrets_access.arn
-}
-
-resource "aws_eks_pod_identity_association" "backend_secrets" {
-  cluster_name    = module.eks.cluster_name
-  namespace       = local.k8s_namespace
-  service_account = "backend-secrets-sa"
-  role_arn        = aws_iam_role.backend_secrets.arn
 }
 
